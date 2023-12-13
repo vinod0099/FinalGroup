25b9b2cb88e8ecb3a5a15e0a03d9340080b8221e
Thummala Sujith <srt7@njit.edu>
2023-12-12 22:14:40 -0500

    hero section added

---
+++ pizzaapp/components/Hero.tsx
@@ -0,0 +1,25 @@
+ // components/Hero.js
+ import Image from 'next/image';
+
+ const Hero = () => {
+   return (
+     <section className="hero grid grid-cols-1 sm:grid-cols-2 gap-8 py-16 mt-4">
+       {/* Left Column */}
+       <div className="col-span-1">
+         <div className="py-16 px-8">
+           <h1 className="text-4xl sm:text-6xl font-bold mb-4">Every Slice Tells a <span className="text-primary">Delicious Story...</span></h1>
+                  
+           <div className="flex flex-col sm:flex-row gap-4">
+       
+           </div>
+         </div>
+       </div>
+ 
+       {/* Right Column */}
+      
+     </section>
+   );
+ };
+ 
+ export default Hero;
+ 
