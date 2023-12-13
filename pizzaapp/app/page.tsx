9c642eeeaefe961ff40dbacb5aa74993e29e5882
Thummala Sujith <srt7@njit.edu>
2023-12-12 21:55:58 -0500

    nav

---
+++ pizzaapp/app/page.tsx
@@ -0,0 +1,18 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import Header from "@/components/Header";
+import '/Users/thummalasujithreddy/Downloads/Webfinal/FinalGroup/pizzaapp/styles/globals.css';
+
+export default function Home() {
+	return (
+		
+			<div className="header-top">
+			<Header/>
+		</div>
+	);
+}
---
+++ pizzaapp/components/Header.tsx
@@ -0,0 +1,45 @@
+'use client'
+// Header.js
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+
+const Header = () => {
+  const openMailchimpSignup = () => {
+    // Replace 'MAILCHIMP_SIGNUP_URL' with the actual Mailchimp signup form URL
+    const mailchimpSignupUrl = 'http://eepurl.com/iFcxPQ';
+    
+    // Open the Mailchimp signup form in a new tab
+    window.open(mailchimpSignupUrl, '_blank');
+  };
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        {/* Your brand content goes here */}
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem >
+          <Link href="#" aria-current="page">
+            Menu
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            AboutUs
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+       
+        
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default Header;
