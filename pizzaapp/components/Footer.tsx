e7726bd5e88aa1dd6d648ae2677b0c7139fc4f6e
Thummala Sujith <srt7@njit.edu>
2023-12-16 18:50:43 -0500

    footer

---
+++ pizzaapp/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-red-600 text-white text-sm lg:text-base w-10">
+      <div className="w-full max-w-full mx-auto py-60 overflow-hidden">
+        {/* Address and contact info */}
+        <div className="hi">
+          
+        </div>
+        {/* Navigation links */}
+        <div className="flex justify-center space-x-4 mb-40">
+          <a href="#ho" className="hover:text-gray-300">HOME</a>
+          <a href="#me" className="hover:text-gray-300">MENU</a>
+         
+        </div>
+        {/* Copyright */}
+        <div className="text-center">
+          <p>&copy; Pizza Hub 2023. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
