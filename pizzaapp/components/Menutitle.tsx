2e4b6f543b0495985de49a5277d986b65a148a76
Thummala Sujith <srt7@njit.edu>
2023-12-16 19:11:30 -0500

    menutitle

---
+++ pizzaapp/components/Menutitle.tsx
@@ -0,0 +1,18 @@
+// MenuTitle.jsx
+const MenuTitle = () => {
+    return (
+      <div className="text-center my-8 md:my-16 items-center">
+        <h2 className="text-5xl md:text-6xl font-extrabold text-orange-600 leading-tight">
+          A Slice of Heaven
+        </h2>
+        <p className="text-2xl md:text-3xl text-gray-700 mt-4">
+          Discover flavors that delight the senses
+        </p>
+       
+      </div>
+    );
+  };
+  
+  export default MenuTitle;
+  
+  
