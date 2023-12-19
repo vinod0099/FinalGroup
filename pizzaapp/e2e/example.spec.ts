9b12a0c77784e9036c7edd0c8d177d7c21a304cb
Sujith Reddy Thummala <144738663+ThummalaSujith@users.noreply.github.com>
2023-12-19 01:50:03 -0500

    Update example.spec.ts

--- pizzaapp/e2e/example.spec.ts
+++ pizzaapp/e2e/example.spec.ts
@@ -1,18 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Navbar is present on the page', async ({ page }) => {
+  await page.goto('https://final-group.vercel.app/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Use the class, ID, or any unique attribute of the Navbar as the 
+  const navbar = await page.$('.navbar'); // Example: If Navbar has a class 'navbar'
+  
+  expect(navbar).not.toBeNull();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+
