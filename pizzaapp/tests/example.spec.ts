0d901077327599efe4306c5e9e662b0eb899e78b
Thummala Sujith <srt7@njit.edu>
2023-12-19 02:07:24 -0500

    update

--- pizzaapp/tests/example.spec.ts
+++ pizzaapp/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Navbar is present on the page', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('https://final-group.vercel.app/');
 
   // Use the class, ID, or any unique attribute of the Navbar as the 
   const navbar = await page.$('.navbar'); // Example: If Navbar has a class 'navbar'
